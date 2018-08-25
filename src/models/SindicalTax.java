917bf030e441f3346917b89a061b07f68c603973
Lucas Agra <lao2@ic.ufal.br>
2018-08-25 10:52:43 -0300

    Modelling

---
+++ src/models/SindicalTax.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class SindicalTax {
+    private Date date;
+    private double value;
+
+    public SindicalTax(Date date, double value) {
+        this.date = date;
+        this.value = value;
+    }
+}
