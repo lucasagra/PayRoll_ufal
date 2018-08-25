ee994dc4e71c397c9a15e779ccac98395611ea43
Lucas Agra <lao2@ic.ufal.br>
2018-08-25 10:53:20 -0300

    Modelling

--- src/models/SyndicalTax.java
+++ src/models/SyndicalTax.java
@@ -2,11 +2,11 @@ package models;
 
 import java.util.Date;
 
-public class SindicalTax {
+public class SyndicalTax {
     private Date date;
     private double value;
 
-    public SindicalTax(Date date, double value) {
+    public SyndicalTax(Date date, double value) {
         this.date = date;
         this.value = value;
     }
